9057965522db605ea146b53655fe334480eb4674
linkedinprojects <githublinkedinprojects@gmail.com>
2024-10-05 11:49:30 +0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
 # tests/test_app.py
+import sys
+import os
+
+# Add the root directory of the project to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app  # Import your Flask app
 
 def test_home_page():
